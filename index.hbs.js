26dcca85fd51936f7ac0a44e17fe1fcb3b4db41e
Mohsen Esmaeili <m-esm@hotmail.com>
2019-06-29 16:42:54 +0430

    new homepage

--- index.hbs.js
+++ index.hbs.js
@@ -6,9 +6,11 @@ async (data, model) => {
   const collection = await modules.sbc.db.collection('Entities', false);
 
   //  res.json(await collection.count({ _entity: 'email' }));
- 
+
   return {
     // handlebars: Modules.handlebars,
-    model: { ok: 'true' }
+    model: {
+      ok: 'true'
+    }
   };
 };
